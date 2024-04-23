342135a7bc6669f4ca50858e282f7c105c9621d3
Sourabh Singh Rajput <sourabhr684@gmail.com>
2024-04-23 19:48:43 +0530

    Added a change File name Function in the application

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path =require('path');
 const fs =require('fs');
+const { log } = require('console');
 
 app.set("view engine","ejs");
 app.use(express.json());
@@ -17,6 +18,14 @@ app.get('/file/:filename', function(req,res){
     fs.readFile(`./files/${req.params.filename}`,"utf-8",function(err,filedata){
         res.render('show',{filename:req.params.filename,filedata:filedata});
     })
+});
+app.get('/edit/:filename', function(req,res){
+    res.render('edit',{filename:req.params.filename});
+});
+app.post('/edit', function(req,res){
+    fs.rename(`./files/${req.body.previousName}`,`./files/${req.body.newName}`,function(err){
+        res.redirect("/");
+    });
 });
     app.post('/create', function(req,res){
         fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
